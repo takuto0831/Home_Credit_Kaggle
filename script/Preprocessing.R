eb823f9498e02272ce4a9ad4d1e1341a64a5f8a2
takuto0831 <aseries2765@gmail.com>
2018-07-26 18:45:14 +0900

    random forest を用いた欠損値補完関数追加

--- script/Preprocessing.R
+++ script/Preprocessing.R
@@ -4,7 +4,7 @@
 # CheckMissingValue: データのfactor列に対して, table関数を実行する. 欠損値の確認
 # dfSummarySplit: factor value -> numeric valueの順に変更して, dfSummary 関数を適用する
 # CheckBinaryColumn: binary value を factor type に変更する(連続値としての情報はない)
-
+# ImputeMissingValueRF: Random Forest による欠損値補完(numeric value)
 ########################################################################################
 
 # CheckMissingValue
@@ -39,5 +39,29 @@ CheckBinaryColumn <- function(col){
   else return(FALSE)
 }
 
+# ImputeMissingValueRF
+ImputeMissingValueRF <- function(data){
+  # remove list
+  patterns <- c("SK_ID","TARGET")
+  # parallel processing
+  cl <- makeCluster(4); registerDoParallel(cl)
+  # impute missing values
+  imp <- data %>% 
+    select_if(!grepl(paste(patterns, collapse="|"),names(.))) %>% # remove contain "SK_ID" or "TARGET"
+    select_if(negate(is.factor)) %>% # only numeric value 
+    as.data.frame() %>% 
+    missForest(
+      variablewise = TRUE, ntree = 1200,
+      parallelize = "forests", verbose = TRUE) 
+  stopCluster(cl)
+  # combine all data
+  ans <- 
+    cbind(data %>% 
+            select_if(grepl(paste(patterns, collapse="|"),names(.))),
+          data %>% 
+            select_if(is.factor),
+          imp$ximp)
+  return(ans)
+}
 
 
