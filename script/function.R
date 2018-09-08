2ce17914abcf13387f0ef13b8e2b0da00ad2d9dd
takuto0831 <aseries2765@gmail.com>
2018-09-08 22:35:11 +0900

    table関数による処理をtabyl関数に変更

--- script/function.R
+++ script/function.R
@@ -1,7 +1,7 @@
 ################################## For  Preprocessing ##################################
 
 ### function list
-# CheckMissingValue: データのfactor列に対して, table関数を実行する. 欠損値の確認
+# CheckMissingValue: データの各列に対して, tabyl関数を適用する. 欠損値の確認, 外れ値の確認を行う.
 # dfSummarySplit: factor value -> numeric valueの順に変更して, dfSummary 関数を適用する
 # CheckBinaryColumn: binary value を factor type に変更する(連続値としての情報はない)
 # CheckCategoryColumn: uniqueな値が100(要考察)以下の場合, カテゴリカル変数とする
@@ -15,14 +15,13 @@
 # CheckMissingValue
 CheckMissingValue <- function(data){
   # Extract factor column
-  data <- data %>% select_if(is.factor)
-  # table function
-  func <- function(col) {
-    table(col,exclude = NULL) %>% 
-      as.data.frame() %>% 
-      kable() %>% print() # print table
-  }
-  apply(data, 2, func)
+  col <- data %>% select_if(is.factor) %>% colnames() 
+  # Extract numeric column
+  col_ <- data %>% select_if(is.factor %>% negate()) %>% colnames() 
+  print("Check factor type value")
+  for (i in 1:length(col)) data %>% tabyl(col[i]) %>% print()
+  print("Check numeric type value")
+  for (i in 1:length(col_)) data %>% tabyl(col_[i]) %>% DT::datatable() %>% print()
 }
 
 # dfSummarySplit
